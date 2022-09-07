625220953decfd4ae0e5824ba26da79a048ba19b
toby <toby@swinson-howell.net>
2022-09-07 16:00:59 +0100

    done!

--- main.py
+++ main.py
@@ -16,8 +16,22 @@ def read():
 def main():
     i = input("read or write (r/w)?")
     if i == "r":
+        chance = 0
         info = read()
-        anger = input("anger level:")
+        # print(info)
+        anger = input("anger level: ")
+        for i in info:
+            # print(i)
+            if i[0] == anger:
+                if i[1] == "1":
+                    chance += 1
+                elif i[1] == "0":
+                    chance -= 1
+        print(chance)
+    if i =="w":
+        anger = input("what anger level(1-10)? ")
+        result = input("what result(0 or 1)? ")
+        write(anger,result)
 
 if __name__ == '__main__':
     main()
